5c4848f6698cfbe4d1fe60ac48e67f5c0063dd0a
PikarryPham <phamngocthuytrang1026@gmail.com>
2022-08-10 00:12:07 +0700

    using venv for python and edit some codes

--- app.py
+++ app.py
@@ -26,13 +26,13 @@ def helloworld():
 '''
 
 
-@app.route('/api/uc10/get-leave-requests', methods=['POST', 'GET'])
+@app.route('/api/uc10/get-leave-requests', methods=['GET','POST'])
 def get_leave_request():
     """
     Example req body
     {
        "limit":"1", //optional
-       "page":"1", //optional
+       "page":"0", //optional
        "employee_id":"1",
        "status":[], 
        "leave_from":"2020-01-01", //optional
@@ -43,119 +43,183 @@ def get_leave_request():
     conn = mysql.connection
     cursor = conn.cursor()
     body_request = request.get_json()
+    # body_request = request.args
+    # print(body_request)
 
-    page = 0  # Trang 1: page 0, Trang 2: page 1....
-    limit = 5  # Mac dinh moi trang co 5 bang ghi
+    page = 0
+    limit = 0
 
     try:
         page = body_request["page"]
+        page = int(page)
     except:
         print("page not found")
 
     try:
         limit = body_request["limit"]
+        limit = int(limit)
     except:
         print("limit not found")
 
-    offset = int(page)*int(limit)
+    offset = int(page) * int(limit)
 
-    employee_id = ""
+    employee_id = ''
     try:
         employee_id = body_request["employee_id"]
     except:
         return "Employee id not found", 500
 
-    leavefrom = "1970-01-01"
+    leavefrom = ''
     try:
         leavefrom = body_request["leave_from"]
-        leavefrom = str(leavefrom)
-        leavefrom = f"'{leavefrom}'"
+        # leavefrom = str(leavefrom)
+        # leavefrom = f"'{leavefrom}'"
     except:
         print("Cannot find leave from of the leave request")
 
-    leaveto = "1970-01-01"
+    leaveto = ''
     try:
         leaveto = body_request["leave_to"]
-        leaveto = str(leaveto)
-        leaveto = f"'{leaveto}'"
+        # leaveto = str(leaveto)
+        # leaveto = f"'{leaveto}'"
     except:
         print("Cannot find leave to of the leave request")
 
-    leave_typeid = "" 
+    leave_typeid = 0
     try:
-        leave_typeid = body_request["leave_type"].lower()
-        switcher = {
-            'annual leave': '1',
-            'personal leave': '2',
-            'compensation leave': '3',
-            'sick leave': '4',
-            'non-paid leave': '5',
-            'maternity leave': '6',
-            'engagement ceremony': '7',
-            'wedding leave': '8',
-            'relative funeral leave': '9'
-        }
-        leave_typeid = switcher.get(leave_typeid, "Invalid leave type id")
+        # leave_typeid = body_request["leave_type"].lower()
+        leave_typeid = body_request["leave_type"]
+        # switcher = {
+        #     'annual leave': '1',
+        #     'personal leave': '2',
+        #     'compensation leave': '3',
+        #     'sick leave': '4',
+        #     'non-paid leave': '5',
+        #     'maternity leave': '6',
+        #     'engagement ceremony': '7',
+        #     'wedding leave': '8',
+        #     'relative funeral leave': '9'
+        # }
+        # leave_typeid = switcher.get(leave_typeid, "Invalid leave type id")
     except:
         print("Cannot find leave type of the leave request")
-        
-    if(leave_typeid == "Invalid leave type id" or leave_typeid == ""):
-        leave_typeid = 0 #mac dinh la lay het leavetype
-    print("Leave type id lan 2 la: " + str(leave_typeid))
-    status = []
+
+    # if(leave_typeid == "Invalid leave type id" or leave_typeid == ""):
+    #     leave_typeid = 0  # mac dinh la lay het leavetype
+    # print("Leave type id lan 2 la: " + str(leave_typeid))
+    status = ''
     try:
         status = body_request["status"]
     except:
-        print("status not found")
+        print("Status not found")
 
-    for i in range(0, len(status)):
-        status[i] = f"'{status[i]}'"
+    if status != '':
+        arr_status = status.split(",")
+
+        for i in range(0, len(arr_status)):
+            arr_status[i] = f"'{arr_status[i]}'"
+
+        search_status = ",".join(arr_status)
+
+    year = ''
+    try:
+        year = body_request["year"]
+    except:
+        print("Year not found")
 
     # Get the record
-    query_string = "SELECT * FROM request_leave "+" WHERE STATUS IN (" + ",".join(
-        status) + ")" + f" AND EMPLOYEE_ID = {employee_id}" + f" AND LEAVE_FROM >= {leavefrom}" + f" AND LEAVE_TO >= {leaveto}" + f" LIMIT {offset},{limit}"
-    print("Do dai cua status" + str(len(status)))
+    # query_string = "SELECT * FROM request_leave "+" WHERE STATUS IN (" + ",".join(
+    #     status) + ")" + f" AND EMPLOYEE_ID = {employee_id}" + f" AND LEAVE_FROM >= {leavefrom}" + f" AND LEAVE_TO >= {leaveto}" + f" LIMIT {offset},{limit}"
+    # print("Do dai cua status" + str(len(status)))
+    # try:
+    #     if(len(status) == 0 and leave_typeid != 0):
+    #         query_string = "SELECT * FROM request_leave " + \
+    #             f" WHERE EMPLOYEE_ID = {employee_id}" + f" AND LEAVE_FROM >= {leavefrom}" + \
+    #             f" AND LEAVE_TO >= {leaveto}" + \
+    #             f" AND LEAVE_TYPE = {leave_typeid}" + \
+    #             f" LIMIT {offset},{limit}"
+    #         print(query_string)
+    #     elif(len(status) == 0 and (leave_typeid == 0 or leave_typeid == '0')):
+    #         query_string = "SELECT * FROM request_leave " + \
+    #             f" WHERE EMPLOYEE_ID = {employee_id}" + f" AND LEAVE_FROM >= {leavefrom}" + \
+    #             f" AND LEAVE_TO >= {leaveto}" + \
+    #             f" LIMIT {offset},{limit}"
+    #         print(query_string)
+    # except:
+    #     return "Status is not array", 500
+    query_string = "SELECT * FROM request_leave " + \
+        f" WHERE EMPLOYEE_ID = {employee_id}"
+
     try:
-        if(len(status) == 0 and leave_typeid != 0):
-            query_string = "SELECT * FROM request_leave " + \
-                f" WHERE EMPLOYEE_ID = {employee_id}" + f" AND LEAVE_FROM >= {leavefrom}" + \
-                f" AND LEAVE_TO >= {leaveto}" + \
-                f" AND LEAVE_TYPE = {leave_typeid}" + \
-                f" LIMIT {offset},{limit}"
-            print(query_string)
-        elif(len(status) == 0 and (leave_typeid == 0 or leave_typeid == '0')):
-            query_string = "SELECT * FROM request_leave " + \
-                f" WHERE EMPLOYEE_ID = {employee_id}" + f" AND LEAVE_FROM >= {leavefrom}" + \
-                f" AND LEAVE_TO >= {leaveto}" + \
-                f" LIMIT {offset},{limit}"
-            print(query_string)
+        if status != '':
+            query_string += f" AND STATUS IN({search_status})"
+
+        if leave_typeid != 0 and leave_typeid != '0':
+            query_string += f" AND LEAVE_TYPE = {leave_typeid}"
+
+        if leavefrom != '':
+            query_string += f" AND LEAVE_FROM >= '{leavefrom}'"
+
+        if leaveto != '':
+            query_string += f" AND LEAVE_TO <= '{leaveto}'"
+
+        if year != '':
+            query_string += f" AND YEAR(CREATE_DATE) = {year}"
+
+        if offset != 0 and limit != 0:
+            query_string += f" LIMIT {offset},{limit}"
+
+        # print(query_string)
     except:
-        return "Status is not array", 500
+        return "Query string has error 1", 500
 
     cursor.execute(query_string)
 
     row_headers = [x[0] for x in cursor.description]
     data = cursor.fetchall()
+
     json_data = []
     for result in data:
         json_data.append(dict(zip(row_headers, result)))
 
     # get the number of total record
-    query_string = "SELECT COUNT(*) FROM request_leave "+" WHERE STATUS IN (" + ",".join(
-        status) + ")" + f" AND EMPLOYEE_ID = {employee_id}" + f" AND LEAVE_FROM >= {leavefrom}" + f" AND LEAVE_TO >= {leaveto} "
+    # query_string = "SELECT COUNT(*) FROM request_leave "+" WHERE STATUS IN (" + ",".join(
+    #     status) + ")" + f" AND EMPLOYEE_ID = {employee_id}" + f" AND LEAVE_FROM >= {leavefrom}" + f" AND LEAVE_TO >= {leaveto} "
+
+    # try:
+    #     if(len(status) == 0 and leave_typeid != 0):
+    #         query_string = "SELECT COUNT(*) FROM request_leave " + f" WHERE EMPLOYEE_ID = {employee_id}" + \
+    #             f" AND LEAVE_FROM >= {leavefrom}" + \
+    #             f" AND LEAVE_TO >= {leaveto}" + \
+    #             f" AND LEAVE_TYPE = {leave_typeid}"
+    #     elif(len(status) == 0 and leave_typeid == 0):
+    #         query_string = "SELECT COUNT(*) FROM request_leave " + f" WHERE EMPLOYEE_ID = {employee_id}" + \
+    #             f" AND LEAVE_FROM >= {leavefrom}" + \
+    #             f" AND LEAVE_TO >= {leaveto}"
+    # except:
+    #     return "System has error", 500
+    query_string = "SELECT COUNT(*) FROM request_leave " + \
+        f" WHERE EMPLOYEE_ID = {employee_id}"
 
     try:
-        if(len(status) == 0 and leave_typeid != 0):
-            query_string = "SELECT COUNT(*) FROM request_leave " + f" WHERE EMPLOYEE_ID = {employee_id}" + \
-                f" AND LEAVE_FROM >= {leavefrom}" + \
-                f" AND LEAVE_TO >= {leaveto}" + \
-                f" AND LEAVE_TYPE = {leave_typeid}"
-        elif(len(status) == 0 and leave_typeid == 0):
-            query_string = "SELECT COUNT(*) FROM request_leave " + f" WHERE EMPLOYEE_ID = {employee_id}" + \
-                f" AND LEAVE_FROM >= {leavefrom}" + \
-                f" AND LEAVE_TO >= {leaveto}"
+        if status != '':
+            query_string += f" AND STATUS IN({search_status})"
+
+        if leave_typeid != 0 and leave_typeid != '0':
+            query_string += f" AND LEAVE_TYPE = {leave_typeid}"
+
+        if leavefrom != '':
+            query_string += f" AND LEAVE_FROM >= '{leavefrom}'"
+
+        if leaveto != '':
+            query_string += f" AND LEAVE_TO <= '{leaveto}'"
+
+        if year != '':
+            query_string += f" AND YEAR(CREATE_DATE) = {year}"
+
+        # print(query_string)
     except:
-        return "System has error", 500
+        return "Query count string has error 2", 500
 
     cursor.execute(query_string)
     total = int(cursor.fetchall()[0][0])
@@ -165,10 +229,11 @@ def get_leave_request():
     return jsonify({
         "total": total,
         "data": json_data,
+        "success": 1
     })
 
 
-@app.route('/api/uc10/delete-a-request', methods=['POST', 'DELETE'])
+@app.route('/api/uc10/delete-a-request', methods=['DELETE','POST'])
 def delete_leavereq():
     """
     Example req body
@@ -180,6 +245,8 @@ def delete_leavereq():
     cursor = conn.cursor()
 
     body_request = request.get_json()
+    #body_request = request.args
+    print(body_request)
 
     rleave_id = ""
     try:
@@ -187,20 +254,51 @@ def delete_leavereq():
     except:
         return "Leave request ID not found. Please enter the Leave request ID", 400
 
-    if(type(rleave_id).__name__ != 'str'):
+    if (type(rleave_id).__name__ != 'str'):
         return "Leave request ID is not a string", 400
 
-    if(len(rleave_id) != 0):
+    if rleave_id != '':
         try:
-            cursor.execute('DELETE FROM request_leave_detail WHERE RLEAVE_ID = %s',
-                           (rleave_id))
+            print('vao day 1')
+            query_string = f"SELECT * FROM request_leave where RLEAVE_ID = {rleave_id}"
 
-            cursor.execute('DELETE FROM request_leave WHERE RLEAVE_ID = %s',
-                           (rleave_id))
+            cursor.execute(query_string)
 
-            conn.commit()
-            return "OK", 200
+            if (len(data) > 0):
+                print('vao day 2')
+                cursor.execute('DELETE FROM request_leave_detail WHERE RLEAVE_ID = %s',
+                               (rleave_id))
+
+                cursor.execute('DELETE FROM request_leave WHERE RLEAVE_ID = %s',
+                               (rleave_id))
+
+                conn.commit()
+                cursor.close()
+
+                return jsonify({
+                    "success": 1,
+                    "data": data
+                })
+
+            return jsonify({
+                "success": 0,
+                "data": []
+            })
         except:
             return "System has error", 500
     else:
         return "System has error when deleting leave request", 500
+
+
+# Thêm cors vào để không bị vi phạm các quy định của cors
+
+@app.after_request
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    header['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    header['Access-Control-Allow-Methods'] = 'OPTIONS, HEAD, GET, POST, DELETE, PUT, PATCH'
+
+    return response
+
+# Ngoài ra ta cần cài đặt các biến venv cho python để đóng gói các package cần thiết
